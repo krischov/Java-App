eed0cc8a6b6e853fc33d6de3cdf52cbced6d50dc
Christopher <calb200@aucklanduni.ac.nz>
2021-05-20 21:45:15 +1200

    create details activity setup and link to list activity

--- app/src/main/java/com/example/carx/DetailsActivity.java
+++ app/src/main/java/com/example/carx/DetailsActivity.java
@@ -1,17 +1,16 @@
 package com.example.carx;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ListActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.list_activity);
+        setContentView(R.layout.details_activity);
         ActionBar actionBar = getSupportActionBar();
 
         if (actionBar != null) {
@@ -19,9 +18,4 @@ public class ListActivity extends AppCompatActivity {
         }
         actionBar.setDisplayHomeAsUpEnabled(true);
     }
-    public void toDetailsActivity(View view) {
-        Intent intent = new Intent(this, DetailsActivity.class);
-        this.startActivity(intent);
-    }
-
 }
