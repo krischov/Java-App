b8cd27560f8d40a64a99823b1e1404629bcc897c
Christopher <calb200@aucklanduni.ac.nz>
2021-05-21 15:30:58 +1200

    make change padding in recycler view

--- app/src/main/java/com/example/carx/Contact.java
+++ app/src/main/java/com/example/carx/Contact.java
@@ -15,10 +15,6 @@ public class Contact {
         return mName;
     }
 
-    public boolean isOnline() {
-        return mOnline;
-    }
-
     private static int lastContactId = 0;
 
     public static ArrayList<Contact> createContactsList(int numContacts) {
